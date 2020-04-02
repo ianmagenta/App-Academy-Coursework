5f302f4de4a8ee952fbc0dfb1d1f73b0926ba808
Ian Magenta <ianmagenta@gmail.com>
2020-04-01 20:18:21 -0500

    Updated Homework

---
+++ wk10d3/sql-recipe-box/data-access-layer/04-insert-new-ingredient.sql
@@ -0,0 +1,17 @@
+-- This query needs to insert new data into the "recipes" table.
+--
+--   * Insert into the "ingredients" table:
+--     * $1 is amount
+--     * $2 is unit of measure id
+--     * $3 is food stuff
+--
+--   * $4 will contain the recipe id
+--
+-- It needs to include all four positional parameters in the VALUES section so
+-- that it inserts the value provided by the user.
+
+
+-- YOUR CODE HERE
+INSERT INTO ingredients (recipe_id, amount, unit_of_measure_id, food_stuff)
+VALUES
+($4, $1, $2, $3);
